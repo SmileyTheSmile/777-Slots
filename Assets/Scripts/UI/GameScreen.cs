3013a081112536400ac42623445a2f704a534ae9
SmileyTheSmile <34063023+SmileyTheSmile@users.noreply.github.com>
2022-06-24 18:26:42 +0200

    Created slot lines filling stuff

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine.UI;
 
@@ -7,6 +8,45 @@ public class GameScreen : GenericMenu
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _exitButton;
     [SerializeField] private TMP_Text _scoreText;
+    [SerializeField] private GameObject _upperLine;
+    [SerializeField] private GameObject _lowerLine;
+    [SerializeField] private List<GameObject> _slotPrefabs;
+    [SerializeField] private int _minElementNum;
+    [SerializeField] private int _defaultBet;
+
+    private List<GameObject> _upperLineElements = new List<GameObject>();
+    private List<GameObject> _lowerLineElements = new List<GameObject>();
+
+    private void Start()
+    {
+        _scoreText.text = _defaultBet.ToString();
+        GenerateSlots();
+    }
+
+    private void GenerateSlots()
+    {
+        for (var i = 0; i < _minElementNum; i++)
+        {
+            var upperSlotElement = Instantiate(_slotPrefabs[Random.Range(0, _slotPrefabs.Count - 1)], _upperLine.transform);
+            _upperLineElements.Add(upperSlotElement);
+            var lowerSlotElement = Instantiate(_slotPrefabs[Random.Range(0, _slotPrefabs.Count - 1)], _lowerLine.transform);
+            _lowerLineElements.Add(lowerSlotElement);
+        }
+    }
+
+    private void ClearLines()
+    {
+        for (var i = _upperLineElements.Count - 1; i >= 0; i--)
+        {
+            Destroy(_upperLineElements[i]);
+            _upperLineElements.RemoveAt(i);
+        }
+        for (var i = _lowerLineElements.Count - 1; i >= 0; i--)
+        {
+            Destroy(_lowerLineElements[i]);
+            _lowerLineElements.RemoveAt(i);
+        }
+    }
 
     protected override void AddAllListeners()
     {
@@ -22,7 +62,8 @@ public class GameScreen : GenericMenu
 
     private void OnPlayButtonClick()
     {
-        
+        ClearLines();
+        GenerateSlots();
     }
 
     private void OnExitButtonClick()
